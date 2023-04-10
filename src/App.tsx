4ddad257f386921c7511dc451cc599c479b95ff4
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-04-10 21:41:54 +0600

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BottomSheetModal from "./BottomSheetModal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div>
+            <button onClick={openModal}>Открыть модальное окно</button>
+            <BottomSheetModal isOpen={isModalOpen} onClose={closeModal}>
+                <h2>Заголовок модального окна</h2>
+                <p>Текст внутри модального окна</p>
+                {/* Вставьте дополнительный контент здесь */}
+            </BottomSheetModal>
+        </div>
+    );
+};
 
 export default App;
