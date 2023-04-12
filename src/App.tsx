24bd8af3b50fb0fe31de9394244da2bf18d7b130
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-04-12 11:20:29 +0600

    Add css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import React, {useState} from "react";
 import BottomSheetModal from "./variant2/BottomSheetModal";
-import styles from "./variant1/BottomSheetModal.module.scss"
+import "./variant2/BottomSheetModal.css"
 
 const App: React.FC = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
 
-    const openModal = () => {
-        setIsModalOpen(true);
+    const handleOpenModal = () => {
+        setModalOpen(true);
     };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
+    const handleCloseModal = () => {
+        setModalOpen(false);
     };
 
     return (
         <div>
-            <div className={styles.centerContainer}>
-                <button className={styles.openButton} onClick={openModal}>
+            <div className="centerContainer">
+                <button className="openButton" onClick={handleOpenModal}>
                     Открыть
                 </button>
             </div>
-
-            <BottomSheetModal isOpen={isModalOpen} onClose={closeModal}>
-                <h2>Заголовок модального окна</h2>
-                <p>Текст внутри модального окна</p>
-                {/* Вставьте дополнительный контент здесь */}
+            <BottomSheetModal isOpen={modalOpen} onClose={handleCloseModal}>
+                <h2>Модальное окно</h2>
+                <p>Здесь может быть ваш контент.</p>
             </BottomSheetModal>
         </div>
     );
